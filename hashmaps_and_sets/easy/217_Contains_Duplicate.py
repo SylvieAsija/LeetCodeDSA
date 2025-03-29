f6e8468a3adcc7c78cad41cd513fdab4381b665c
SylvieAsija <19aa10@queensu.ca>
2025-03-28 20:46:06 -0400

    contains duplicate

---
+++ hashmaps_and_sets/easy/217_Contains_Duplicate.py
@@ -0,0 +1,56 @@
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+'''
+'''
+SOLUTION
+Runtime: 11ms O(n) -> 3ms -> beats 76.18%
+Memory:               25.90MB -> beats 44.21%
+'''
+
+
+def containsDuplicate(nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    my_set = set()
+    for num in nums:
+        if num in my_set:
+            return True
+        my_set.add(num)
+    return False
